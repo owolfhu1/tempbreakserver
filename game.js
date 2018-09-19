5e8d11d6a6b2479ed5281bd490467581a190fcab
owolfhu1 <owolfhu1@gmail.com>
2018-09-19 11:44:20 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,6 +2,13 @@ const Constants = require('./constants/Constants');
 const levels = require('./levels');
 const SSCD = require('sscd').sscd;
 
+const shuffle = a => {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+};
 
 function Player(color) {
     this.points = 0;
