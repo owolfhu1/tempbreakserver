414de570c7c96ec7f0d31c2add21dd6a57574519
owolfhu1 <owolfhu1@gmail.com>
2018-09-19 09:00:41 -0400

    add shuffle function

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ const users = {};
 const games = {};
 const gamePlayers = {};
 
+const shuffle = a => {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+};
+
 const login = (name, id) => {
     if (!users[name]) {
         lobby[name] = id;
